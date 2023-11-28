eb9417dc566dd857b16c148dab32316c2ffa37e3
Nuria <nmorras@hotmail.com>
2023-11-29 00:01:51 +0100

    added job

--- you.py
+++ you.py
@@ -1,5 +1,7 @@
 import My
 import New_feat
+import job
 
-print(f"Hello {My.nombre} {New_feat.apellido} {New_feat.apellido2}")
+print(f"Hello {My.nombre} {New_feat.apellido} {New_feat.apellido2} {job.job}")
+
 
